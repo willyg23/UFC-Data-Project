152ce5de1af87519e64d5bc39b985dc113b5323c
willyg23 <grinerwill5@gmail.com>
2024-03-14 15:36:08 -0500

    _fighters hashmap and _fights list created as the app boots up

--- json_test/lib/main.dart
+++ json_test/lib/main.dart
@@ -504,28 +504,28 @@ maybe have anothe box appear for the input to be positive or negative?
     });
   }
 
-  @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text(widget.title),
-    ),
-    body: _isLoading 
-        ? Center(child: CircularProgressIndicator())  // Loading state
-        : Column( 
-            children: [
-              Expanded( 
-                 child: charts.LineChart(
-                   _generateChartData().cast<charts.Series<dynamic, num>>(),
-                   animate: true,
-                   domainAxis: charts.DateTimeAxisSpec(),
-                 ),
-              ),
-              // ... Other widgets below the graph ...
-            ],
-          ), 
-  );
-}
+//   @override
+// Widget build(BuildContext context) {
+//   return Scaffold(
+//     appBar: AppBar(
+//       title: Text(widget.title),
+//     ),
+//     body: _isLoading 
+//         ? Center(child: CircularProgressIndicator())  // Loading state
+//         : Column( 
+//             children: [
+//               Expanded( 
+//                  child: charts.LineChart(
+//                    _generateChartData().cast<charts.Series<dynamic, num>>(),
+//                    animate: true,
+//                    domainAxis: charts.DateTimeAxisSpec(),
+//                  ),
+//               ),
+//               // ... Other widgets below the graph ...
+//             ],
+//           ), 
+//   );
+// }
 
 
 // @override
@@ -550,31 +550,31 @@ Widget build(BuildContext context) {
 // }
 
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   // This method is rerun every time setState is called, for instance as done
-  //   // by the _incrementCounter method above.
-  //   //
-  //   // The Flutter framework has been optimized to make rerunning build methods
-  //   // fast, so that you can just rebuild anything that needs updating rather
-  //   // than having to individually change instances of widgets.
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       // TRY THIS: Try changing the color here to a specific color (to
-  //       // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-  //       // change color while the other colors stay the same.
-  //       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-  //       // Here we take the value from the MyHomePage object that was created by
-  //       // the App.build method, and use it to set our appbar title.
-  //       title: Text(widget.title),
-  //     ),
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
      
-  //     body: ElevatedButton(
-  //       onPressed: () {
-  //         readJson();
-  //       },
-  //       child: Center(child: Text("Load Json"))),
+      body: ElevatedButton(
+        onPressed: () {
+          
+        },
+        child: Center(child: Text("yay!"))),
 
-  //   );
-  // }
+    );
+  }
 }
